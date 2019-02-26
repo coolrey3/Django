c111375b385bd6abf8bac641853d6edcabf87e93
Ray Mercedes <contact@RayMercedes.com>
2019-02-26 00:29:41 -0500

    for loop

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Ray',
+        'title': 'first blog',
+        'content': 'First blog content',
+        'date_posted':'March 1,2019'
+    },
+    {
+        'author': 'mel',
+        'title': 'second blog',
+        'content': 'second blog content',
+        'date_posted': 'March 2,2019'
+    },
+    {
+        'author': 'Suzy',
+        'title': 'third blog',
+        'content': 'third blog content',
+        'date_posted': 'March 4,2019'
+    }
+
+]
+
 def home(request):
-    return render(request,'blog/home.html')
+    context = {
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
     return render(request,'blog/about.html')
